a486402314729caf432dfede96a410d057c01d25
Zetian_Chang <Zetian_Chang@trendmicro.com>
2017-11-29 21:10:02 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'my website' });
-});
+// router.get('/', function(req, res, next) {
+//   // res.render('index', { title: 'my website' });
+//   res.
+// });
 
 module.exports = router;
